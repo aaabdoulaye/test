03c620e76ce2106c890d24653e4cc3d398fd9483
abdoul aziz <asouabdoulaye@yahoo.fr>
2012-12-24 11:37:33 +0000

    un commit pour test

---
+++ FirstActivity/src/sn/esmt/act/FirstActivityActivity.java
@@ -0,0 +1,13 @@
+package sn.esmt.act;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirstActivityActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
